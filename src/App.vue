9b0ce15a19a16a3d387ebb678c10b9a514b1ffd7
Gianluca <gianluca.nesta@gmail.com>
2023-10-16 22:19:09 +0200

    basic css styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="wrapper">
+    <h1 class="header">Hello, I'm in App.vue</h1>
     <HelloWorld msg="You did it!" />
-    <HelloWorld msg="Nice job!" />
   </div>
 </template>
 
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
+
+<style>
+.head {
+  color: purple;
+}
+</style>
