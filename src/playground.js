83bba08d0e957650757995ddeab8a6c0a022bb80
Gianluca <gianluca.nesta@gmail.com>
2023-10-17 22:13:03 +0200

    BONUS: JavaScript Objects, Properties, and Methods

--- src/playground.js
+++ src/playground.js
@@ -1,5 +1,20 @@
-const fruits = ['Apple', 'Banana', 'Orange']
+const developer = {
+  salary: 100000,
+  experience: 4.5,
+  techStack: ['Vue', 'Html', 'Css'],
+  lookingForWork: true,
+  doubleSalary() {
+    this.salary = this.salary * 2
+    this.lookingForWork = false
+  }
+}
+console.log('🚀 ~ file: playground.js:7 ~ developer:', developer.salary)
+console.log('🚀 ~ file: playground.js:7 ~ developer:', developer.lookingForWork)
 
-const vegetables = ['Cucumber', 'Radish']
+developer.doubleSalary()
 
-const concatArr = [...fruits, ...vegetables]
+console.log('🚀 ~ file: playground.js:7 ~ developer:', developer.salary)
+console.log('🚀 ~ file: playground.js:7 ~ developer:', developer.lookingForWork)
+
+// using the command --> node src/playground.js
+// you can visualize the console.log on the terminal
