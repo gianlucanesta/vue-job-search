bfca3671738ee8915270dd5287416c27a5d29ca0
Gianluca <gianluca.nesta@gmail.com>
2023-10-17 22:20:29 +0200

    Rendering Data to View I

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,13 +2,18 @@
   <header class="w-full text-sm">Main Nav</header>
   <div class="fixed top-0 left-0 w-full h-16 bg-white">
     <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1">
-      <a class="flex h-full items-center text-xl" href="/">My Career</a>
+      <a class="flex h-full items-center text-xl" href="/">{{ company }}</a>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MainNav'
+  name: 'MainNav',
+  data() {
+    return {
+      company: 'My Career'
+    }
+  }
 }
 </script>
