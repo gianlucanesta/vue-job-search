a4c3e6e0e39992e034bf5cda881c49e800a04dd2
Gianluca <gianluca.nesta@gmail.com>
2023-10-17 22:25:09 +0200

    Rendering Data to View II: Multiple Data Properties

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,6 +3,9 @@
   <div class="fixed top-0 left-0 w-full h-16 bg-white">
     <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1">
       <a class="flex h-full items-center text-xl" href="/">{{ company }}</a>
+      <h2 class="ml-8 flex h-full items-center">
+        Developed by {{ author.firstName }} {{ author.lastName }}
+      </h2>
     </div>
   </div>
 </template>
@@ -12,7 +15,11 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'My Career'
+      company: 'My Career',
+      author: {
+        firstName: 'Gianluca',
+        lastName: 'Nesta'
+      }
     }
   }
 }
