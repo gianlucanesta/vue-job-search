8568f2fe432962ad75f65070e10fadad2ab679d2
Gianluca <gianluca.nesta@gmail.com>
2023-10-20 16:30:54 +0200

    Multiple Describe Blocks

--- src/playground.js
+++ src/playground.js
@@ -1,20 +1,28 @@
-const developer = {
-  salary: 100000,
-  experience: 4.5,
-  techStack: ['Vue', 'Html', 'Css'],
-  lookingForWork: true,
-  doubleSalary() {
-    this.salary = this.salary * 2
-    this.lookingForWork = false
-  }
-}
-console.log('🚀 ~ file: playground.js:7 ~ developer:', developer.salary)
-console.log('🚀 ~ file: playground.js:7 ~ developer:', developer.lookingForWork)
+// const developer = {
+//   salary: 100000,
+//   experience: 4.5,
+//   techStack: ['Vue', 'Html', 'Css'],
+//   lookingForWork: true,
+//   doubleSalary() {
+//     this.salary = this.salary * 2
+//     this.lookingForWork = false
+//   }
+// }
+// console.log('🚀 ~ file: playground.js:7 ~ developer:', developer.salary)
+// console.log('🚀 ~ file: playground.js:7 ~ developer:', developer.lookingForWork)
 
-developer.doubleSalary()
+// developer.doubleSalary()
 
-console.log('🚀 ~ file: playground.js:7 ~ developer:', developer.salary)
-console.log('🚀 ~ file: playground.js:7 ~ developer:', developer.lookingForWork)
+// console.log('🚀 ~ file: playground.js:7 ~ developer:', developer.salary)
+// console.log('🚀 ~ file: playground.js:7 ~ developer:', developer.lookingForWork)
 
 // using the command --> node src/playground.js
 // you can visualize the console.log on the terminal
+
+export const evenOrOdd = (number) => {
+  if (number % 2 === 0) {
+    return 'Even'
+  } else {
+    return 'Odd'
+  }
+}
--- tests/unit/playground.test.js
+++ tests/unit/playground.test.js
@@ -1,7 +1,21 @@
 import { describe, it, expect } from 'vitest'
 
+import { evenOrOdd } from '../../src/playground'
+
 describe('basic math', () => {
   it('adds two numbers', () => {
     expect(1 + 1).toBe(2)
+  })
+})
+
+describe('evenOrOdd', () => {
+  it('indicates the number is even', () => {
+    expect(evenOrOdd(4)).toBe('Even')
+  })
+})
+
+describe('evenOrOdd', () => {
+  it('indicates the number is odd', () => {
+    expect(evenOrOdd(3)).toBe('Odd')
   })
 })
