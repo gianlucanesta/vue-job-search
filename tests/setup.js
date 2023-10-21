e0cf68d32ed082c6cc3a1bf4a39456a34c801f85
Gianluca <gianluca.nesta@gmail.com>
2023-10-21 22:29:44 +0200

    The render Function and screen Object

--- tests/setup.js
+++ tests/setup.js
@@ -2,7 +2,7 @@ import { cleanup } from '@testing-library/vue'
 import matchers from '@testing-library/jest-dom/matchers'
 import { expect, afterEach } from 'vitest'
 
-expect.extend(matchers)
+// expect.extend(matchers)
 
 afterEach(() => {
   cleanup()
---
+++ tests/unit/components/MainNav.test.js
@@ -0,0 +1,11 @@
+// import { render, screen } from '@testing-library/vue'
+
+import { render, screen } from '@testing-library/vue'
+import MainNav from '@/components/MainNav.vue'
+
+describe('MainNav', () => {
+  it('displays company name', () => {
+    render(MainNav)
+    screen.debug()
+  })
+})
