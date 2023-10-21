5b25a1917624dbb806a8be72ca603800efc3ee1c
Gianluca <gianluca.nesta@gmail.com>
2023-10-21 19:26:34 +0200

    Cleaning up App.vue

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,9 +3,6 @@
   <div class="fixed top-0 left-0 w-full h-16 bg-white">
     <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1">
       <a class="flex h-full items-center text-xl" href="/">{{ company }}</a>
-      <h2 class="ml-8 flex h-full items-center">
-        Developed by {{ author.firstName }} {{ author.lastName }}
-      </h2>
     </div>
   </div>
 </template>
@@ -15,11 +12,7 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'My Career',
-      author: {
-        firstName: 'Gianluca',
-        lastName: 'Nesta'
-      }
+      company: 'My Career'
     }
   }
 }
