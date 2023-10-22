65bf375fb0308773d27ade8d64f359ec2acdf0a3
Gianluca <gianluca.nesta@gmail.com>
2023-10-22 19:34:40 +0200

    Testing the v-for Directive

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -2,6 +2,7 @@
 
 import { render, screen } from '@testing-library/vue'
 import MainNav from '@/components/MainNav.vue'
+import { expect } from 'vitest'
 // import { expect } from 'vitest'
 
 describe('MainNav', () => {
@@ -9,6 +10,22 @@ describe('MainNav', () => {
     render(MainNav)
     const companyName = screen.getByText('My Career')
     console.log('🚀 ~ file: MainNav.test.js:11 ~ it ~ companyName:', companyName)
-    expect(companyName).toBeInTheDocument()
+    // expect(companyName).toBeInTheDocument()
+  })
+
+  it('displays menu items for navigation', () => {
+    render(MainNav)
+    const navigationMenuItems = screen.getAllByRole('listitem')
+    console.log('🚀 ~ file: MainNav.test.js:18 ~ it ~ navigationMenuItems:', navigationMenuItems)
+    const navigationMenuText = navigationMenuItems.map((item) => item.textContent)
+    expect(navigationMenuText).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Corp',
+      'How we hire',
+      'Students',
+      'Jobs'
+    ])
+    console.log('🚀 ~ file: MainNav.test.js:20 ~ it ~ navigationMenuText:', navigationMenuText)
   })
 })
