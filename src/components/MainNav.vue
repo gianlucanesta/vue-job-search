22c098053ed9ce93541ea2dcae789394d93cf02f
Gianluca <gianluca.nesta@gmail.com>
2023-10-22 15:03:01 +0200

    The v-bind Directive I

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,7 @@
   <header class="w-full text-sm">Main Nav</header>
   <div class="fixed top-0 left-0 w-full h-16 bg-white">
     <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1">
-      <a class="flex h-full items-center text-xl" href="/">{{ company }}</a>
+      <a class="flex h-full items-center text-xl" :href="url">{{ company }}</a>
     </div>
   </div>
 </template>
@@ -12,7 +12,8 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'My Career'
+      company: 'My Career',
+      url: 'https://careers.google.com'
     }
   }
 }
