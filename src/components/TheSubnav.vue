1e96ce7a7cbacd0ba94784ef844e3fabb93cb576
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-03-26 19:47:25 +0100

    Using TDD for v-if Directive for Jobs Page

--- src/components/TheSubnav.vue
+++ src/components/TheSubnav.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="h-16 w-full border-b border-solid border-brand-gray-1">
     <div class="h-full flex items-center px-8">
-      <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
-      <span> <span class="text-brand-green-1">1653</span> jobs matched</span>
+      <div v-if="onJobResultsPage">
+        <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
+        <span> <span class="text-brand-green-1">1653</span> jobs matched</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TheSubnav'
+  name: 'TheSubnav',
+  data() {
+    return {
+      onJobResults: true
+    }
+  }
 }
 </script>
---
+++ tests/unit/components/TheSubnav.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/vue'
+
+import TheSubnav from '@/components/TheSubnav.vue'
+
+describe('TheSubnav', () => {
+  describe("when the user is on the 'Jobs' page", () => {
+    it('displays jobs count', () => {
+      render(TheSubnav, {
+        data() {
+          return {
+            onJobResultsPage: true
+          }
+        }
+      })
+      const jobCount = screen.getByText('1653')
+      // expect(jobCount).toBeInTheDocument()
+    })
+  })
+
+  // describe("when the user is not on the 'Jobs' page", () => {
+  //   it('does NOT displays jobs count', () => {
+  //     render(TheSubnav, {
+  //       data() {
+  //         return {
+  //           onJobResultsPage: false
+  //         }
+  //       }
+  //     })
+  //     const jobCount = screen.queryByText('1653')
+  //     expect(jobCount).not.toBeInTheDocument()
+  //   })
+  // })
+})
