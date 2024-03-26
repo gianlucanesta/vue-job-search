b17897a1c3e118f4696912c866d84729cfcf0b7e
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-03-26 19:16:17 +0100

    Styling TheSubnav Component

--- src/components/TheSubnav.vue
+++ src/components/TheSubnav.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>This is a subnav</div>
+  <div class="h-16 w-full border-b border-solid border-brand-gray-1">
+    <div class="h-full flex items-center px-8">This is a subnav</div>
+  </div>
 </template>
 
 <script>
