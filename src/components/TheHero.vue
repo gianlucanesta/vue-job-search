d37bccc4a6de10d8bf3e9f2c6bafa4a9fc750d1b
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-03-27 19:43:51 +0100

    Using the created Lifecycle Hook

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -3,7 +3,7 @@
     <section class="flex h-screen flex-col bg-yellow-200 pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1 border border-blue-700">1 column</div>
-        <div class="col-span-5 col-start-2 border border-blue-700">5 column</div>
+        <div class="col-span-5 col-start-2 border border-blue-700"><the-headline /></div>
         <div class="col-span-5 col-start-7 border border-blue-700">5 column</div>
         <div class="col-span-1 col-start-12 border border-blue-700">1 column</div>
       </div>
@@ -11,19 +11,11 @@
   </main>
 </template>
 <script>
+import TheHeadline from './TheHeadline.vue'
 export default {
   name: 'TheHero',
-  beforeCreate() {
-    console.log('TheHero beforeCreate')
-  },
-  created() {
-    console.log('TheHero created')
-  },
-  beforeMount() {
-    console.log('TheHero beforeMount')
-  },
-  mounted() {
-    console.log('TheHero mounted')
+  components: {
+    TheHeadline
   }
 }
 </script>
