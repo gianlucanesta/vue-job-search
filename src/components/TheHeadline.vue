03d6e778356a87fc3786d9b67821081e11ccc834
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-03-27 20:53:22 +0100

    Moving Interval Function Logic Out of Component

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -32,13 +32,7 @@ export default {
   },
   methods: {
     changeTitle() {
-      this.interval = setInterval(() => {
-        const actions = ['Build', 'Create', 'Design', 'Code']
-        const currentActionIndex = actions.indexOf(this.action)
-        const nextActionIndex = (currentActionIndex + 1) % actions.length
-        const nextAction = actions[nextActionIndex]
-        this.action = nextAction
-      }, 3000)
+      this.interval = setInterval(() => {}, 3000)
     }
   }
 }
