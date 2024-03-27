9683b6ffaf205edc17af2a52555529eba22bcbf3
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-03-27 21:55:19 +0100

    Tests for TheHeadline Component

--- tests/unit/components/TheHeadline.test.js
+++ tests/unit/components/TheHeadline.test.js
@@ -3,11 +3,23 @@ import { render, screen } from '@testing-library/vue'
 import TheHeadline from '@/components/TheHeadline.vue'
 
 describe('TheHeadline', () => {
-  describe('Vitest playground', () => {
-    it('tracks whether it has been called', () => {
-      const mockFunction = vi.fn()
-      mockFunction(1, 2, 3)
-      expect(mockFunction).toHaveBeenCalledWith(1, 2, 3)
+  it('displays introductory action verb', () => {
+    vi.useFakeTimers()
+    render(TheHeadline)
+
+    const actionPhrase = screen.getByRole('heading', {
+      name: /build for everyone/i
     })
+    // expect(actionPhrase).toBeInTheDocument()
+    vi.useRealTimers()
+  })
+
+  it('changes action verb at a consistent interval', () => {
+    vi.useFakeTimers()
+    const mock = vi.fn()
+    vi.stubGlobal('setInterval', mock)
+    render(TheHeadline)
+
+    expect(mock).toHaveBeenCalled()
   })
 })
