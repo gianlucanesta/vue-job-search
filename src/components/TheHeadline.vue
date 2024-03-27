a6ef503f06fa0ddbaf8756be5624e14b8ea9f43c
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-03-27 20:34:05 +0100

    CSS: Fix Styles of Primary Header

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
-    <h1>
-      <span :class="actionClasses">{{ action }}</span>
+    <h1 class="mb-14 text-8xl font-bold tracking-tighter">
+      <span :class="actionClasses">{{ action }}</span> <br />
       for everyone
     </h1>
     <h2>Find your next job</h2>
