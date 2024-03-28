ec5c9f3606a1ce9a4e249e19c05d0e8a0e18b80d
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-03-28 18:50:25 +0100

    CSS: Styling JobSearchForm II

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -34,14 +34,14 @@ export default {
 </script>
 <style scoped>
 button {
-  @apply rounded px-5 py-3 font-medium;
+  @apply px-5 py-3 font-medium;
 }
 
 .primary {
-  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+  @apply rounded bg-brand-blue-1 text-white hover:shadow-blue;
 }
 
 .secondary {
-  @apply bg-transparent text-brand-blue-1 hover:bg-brand-blue-2  hover:text-white;
+  @apply font-semibold bg-transparent text-brand-blue-1 hover:bg-brand-blue-2  hover:text-white;
 }
 </style>
