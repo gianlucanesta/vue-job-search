552fa65dcb55543c2f9e352f5943551e733aaebe
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-03-30 16:51:17 +0100

    The router-view Component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <main-nav />
-  <the-hero />
+  <router-view></router-view>
 </template>
 
 <script>
-import MainNav from '@/components/Navigation/MainNav.vue'
-import TheHero from '@/components/JobSearch/TheHero.vue'
-
 export default {
-  name: 'App',
-  components: {
-    MainNav,
-    TheHero
-  }
+  name: 'App'
 }
 </script>
