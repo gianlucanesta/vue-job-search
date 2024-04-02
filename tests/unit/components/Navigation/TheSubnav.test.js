0d0302d87f16fb6cae695a7b69114aa967cd612e
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-02 23:29:21 +0200

    Using Factory Functions for TheSubnav Tests

--- tests/unit/components/Navigation/TheSubnav.test.js
+++ tests/unit/components/Navigation/TheSubnav.test.js
@@ -3,19 +3,22 @@ import { render, screen } from '@testing-library/vue'
 import TheSubnav from '@/components/Navigation/TheSubnav.vue'
 
 describe('TheSubnav', () => {
+  const renderTheSubnav = (routeName) => {
+    render(TheSubnav, {
+      global: {
+        mocks: {
+          $route: { name: routeName }
+        },
+        stubs: {
+          FontAwesomeIcon: true
+        }
+      }
+    })
+  }
   describe("when the user is on the 'Jobs' page", () => {
     it('displays jobs count', () => {
-      const $route = { name: 'JobResults' }
-      render(TheSubnav, {
-        global: {
-          mocks: {
-            $route: $route
-          },
-          stubs: {
-            FontAwesomeIcon: true
-          }
-        }
-      })
+      const routeName = 'JobResults'
+      renderTheSubnav(routeName)
       const jobCount = screen.getByText('1653')
       // expect(jobCount).toBeInTheDocument()
     })
@@ -23,17 +26,9 @@ describe('TheSubnav', () => {
 
   describe("when the user is not on the 'Jobs' page", () => {
     it('does NOT displays jobs count', () => {
-      const $route = { name: 'Home' }
-      render(TheSubnav, {
-        global: {
-          mocks: {
-            $route: { name: 'Home' }
-          },
-          stubs: {
-            FontAwesomeIcon: true
-          }
-        }
-      })
+      const routeName = 'Home'
+      renderTheSubnav(routeName)
+
       const jobCount = screen.queryByText('1653')
       // expect(jobCount).not.toBeInTheDocument()
     })
