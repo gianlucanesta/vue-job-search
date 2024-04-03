a9fe2e8577221d328810c461e2e15630a398b4f0
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-03 19:47:51 +0200

    Extracting JobListing to Separate Component

---
+++ src/components/JobResults/JobListing.vue
@@ -0,0 +1,40 @@
+<template>
+  <li class="mb-7">
+    <router-link
+      to="/jobs/results/1"
+      class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
+    >
+      <div class="mx-8 border-b border-solid border-brand-gray-2 pt-5 pb-2">
+        <h2 class="mb-2 text-2xl">Technical Program Manager, Perception, Augmented Reality</h2>
+        <div class="flex flex-row align-middle">
+          <div class="mr-5"><span>Meta</span></div>
+          <div class="mr-5"><span>San Francisco, CA, USA</span></div>
+        </div>
+
+        <div class="px-8 py-4">
+          <div>
+            <h3 class="mt-1 mb-2">Qualifications:</h3>
+            <div>
+              <ul class="list-disc pl-8">
+                <li>Bachelors or Masters in Computer Science</li>
+                <li>5+ years of experience in a similar role</li>
+                <li>Excellent written and verbal communication skills</li>
+                <li>Ability to work well in a team</li>
+                <li>Strong attention to detail</li>
+              </ul>
+            </div>
+          </div>
+          <div class="mt-2 text-center">
+            <router-link to="/jobs/results/1" class="text-brand-blue-1">Expand</router-link>
+          </div>
+        </div>
+      </div>
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'JobListing'
+}
+</script>
