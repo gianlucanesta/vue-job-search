4d8c4204f26e78234d78fe2d0add4428dc9767e6
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-03 22:21:07 +0200

    Use async/await Syntax to Fetch Jobs

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <job-listing v-for="job in 10" :key="job.id" />
+      <job-listing v-for="job in jobs" :key="job.id" job="job" />
     </ol>
   </main>
 </template>
@@ -19,10 +19,9 @@ export default {
       jobs: []
     }
   },
-  mounted() {
-    axios.get('http://localhost:5000/jobs').then((response) => {
-      this.jobs = response.data
-    })
+  async mounted() {
+    const response = await axios.get('http://localhost:5000/jobs')
+    this.jobs = response.data
   }
 }
 </script>
