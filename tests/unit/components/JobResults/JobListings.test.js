90f4a8497cc366f1c246c6f395ea0b51f17e5951
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-04 21:22:19 +0200

    Create First Tests for JobListings Component

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -1,11 +1,32 @@
 import { render, screen } from '@testing-library/vue'
 import JobListings from '@/components/JobResults/JobListings.vue'
 import axios from 'axios'
+import { RouterLinkStub } from '@vue/test-utils'
 
-console.log(axios)
 vi.mock('axios')
 describe('JobListings', () => {
-  it('renders', () => {
-    expect(1).toBe(1);
+  it('fetches jobs', () => {
+    axios.get.mockResolvedValue({
+      data: {
+        jobs: []
+      }
+    })
+    render(JobListings)
+    expect(axios.get).toHaveBeenCalledWith('http://localhost:5000/jobs')
+  })
+  it('creates a job listing for every job', async () => {
+    axios.get.mockResolvedValue({
+      data: Array(15).fill({})
+    })
+    render(JobListings, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+
+    const jobListings = await screen.findAllByRole('listitem')
+    expect(jobListings).toHaveLength(15)
   })
 })
