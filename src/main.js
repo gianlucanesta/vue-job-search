501cdf9eedeae4f410f561413d446d14873125e6
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-06 16:30:38 +0200

    Register Pinia Store

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import '@/index.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faSearch)
+
+const pinia = createPinia()
 import router from '@/router'
 import App from '@/App.vue'
 
-createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(pinia).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
