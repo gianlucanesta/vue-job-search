830c8c0ea4f920449e97a58998f893834ef20431
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-06 14:52:39 +0200

    Adding Tests for Page Number

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -49,4 +49,26 @@ describe('JobListings', () => {
     const jobListings = await screen.findAllByRole('listitem')
     expect(jobListings).toHaveLength(10)
   })
+
+  describe('when params exclude page number', () => {
+    it('displays page number 1', () => {
+      const queryParams = { page: undefined }
+      const $route = createRoute(queryParams)
+
+      renderJobListings($route)
+
+      // expect(screen.getByText('Page 1')).toBeInTheDocument()
+    })
+  })
+
+  describe('when params include page number', () => {
+    it('displays page number', () => {
+      const queryParams = { page: 2 }
+      const $route = createRoute(queryParams)
+
+      renderJobListings($route)
+
+      // expect(screen.getByText('Page 2')).toBeInTheDocument()
+    })
+  })
 })
