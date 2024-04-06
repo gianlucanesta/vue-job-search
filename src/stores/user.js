19cf0809acf2e67bc3b97e27a5731f7f0cd13edc
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-06 17:05:07 +0200

    Add First Store Action

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,9 +1,17 @@
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', {
-  state: () => {
+  state: () => ({
     return {
       isLoggedIn: false
+    }
+  }),
+  actions: {
+    loginUser() {
+      this.isLoggedIn = true
+    },
+    logoutUser() {
+      this.isLoggedIn = false
     }
   }
 })
