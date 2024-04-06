1ca4999de9ffa75524dcf97548c6d80b5f71317d
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-06 20:02:41 +0200

    Dispatching Action from Component to Fetch Jobs

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -31,18 +31,15 @@
 </template>
 
 <script>
-import axios from 'axios'
+import { mapActions, mapState } from 'pinia'
 import JobListing from '@/components/JobResults/JobListing.vue'
+import { useJobsStore, FETCH_JOBS } from '@/stores/jobs'
 export default {
   name: 'JobListings',
   components: {
     JobListing
   },
-  data() {
-    return {
-      jobs: []
-    }
-  },
+
   computed: {
     currentPage() {
       return Number.parseInt(this.$route.query.page || 1)
@@ -52,27 +49,31 @@ export default {
       const firstPage = 1
       return previousPage >= firstPage ? previousPage : undefined
     },
-    nextPage() {
-      const nextPage = this.currentPage + 1
-      const maxPage = Math.ceil(this.jobs.length / 10)
+    ...mapState(useJobsStore, {
+      jobs: 'jobs',
+      nextPage() {
+        const nextPage = this.currentPage + 1
+        const maxPage = Math.ceil(this.jobs.length / 10)
 
-      return this.currentPage < maxPage ? nextPage : undefined
-    },
-    displayedJobs() {
-      const pageNumber = this.currentPage
-      const firstJobIndex = (pageNumber - 1) * 10
-      const lastJobIndex = firstJobIndex + 10
-      if (this.jobs.length > 0) {
-        return this.jobs.slice(firstJobIndex, lastJobIndex)
-      } else {
-        return []
+        return this.currentPage < maxPage ? nextPage : undefined
+      },
+      displayedJobs() {
+        const pageNumber = this.currentPage
+        const firstJobIndex = (pageNumber - 1) * 10
+        const lastJobIndex = firstJobIndex + 10
+        if (this.jobs.length > 0) {
+          return this.jobs.slice(firstJobIndex, lastJobIndex)
+        } else {
+          return []
+        }
       }
-    }
+    })
   },
   async mounted() {
-    const baseUrl = import.meta.env.VITE_APP_API_URL
-    const response = await axios.get(`${baseUrl}/jobs`)
-    this.jobs = response.data
+    this.FETCH_JOBS()
+  },
+  methods: {
+    ...mapActions(useJobsStore, [FETCH_JOBS])
   }
 }
 </script>
