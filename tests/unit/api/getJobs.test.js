283f1e614da60883c1bf75cdb1f355fca9a3b200
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-06 19:01:15 +0200

    TDD: Adding Test for Jobs API Call I

--- tests/unit/api/getJobs.test.js
+++ tests/unit/api/getJobs.test.js
@@ -1,3 +1,11 @@
 import axios from 'axios'
+import getJobs from '@/api/getJobs'
+
 vi.mock('axios')
-console.log(axios)
+
+describe('getJobs', () => {
+  it('fetches jobs that candidates can apply to', async () => {
+    await getJobs()
+    expect(axios.get).toHaveBeenCalledWith('http://localhost:5000/jobs')
+  })
+})
