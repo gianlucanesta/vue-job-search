fb86737b7655984314d615b6cfbed614866e0a95
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-06 18:05:20 +0200

    Fixing our Failing MainNav Tests

--- tests/unit/components/Navigation/MainNav.test.js
+++ tests/unit/components/Navigation/MainNav.test.js
@@ -1,14 +1,17 @@
 import { render, screen } from '@testing-library/vue'
 import userEvent from '@testing-library/user-event'
 import { RouterLinkStub } from '@vue/test-utils'
+import { createTestingPinia } from '@pinia/testing'
 
 import MainNav from '@/components/Navigation/MainNav.vue'
 
 describe('MainNav', () => {
   const renderMainNav = () => {
+    const pinia = createTestingPinia({ stubActions: false })
     const $route = { name: 'Home' }
     render(MainNav, {
       global: {
+        plugins: [pinia],
         mocks: {
           $route: $route
         },
