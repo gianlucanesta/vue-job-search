20cce17793820bf283ca2fbf9f4d9ed54a2c489e
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-07 18:05:17 +0200

    Use v-for to Render Unique Organizations in JobFiltersSidebarOrganizations

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -2,25 +2,10 @@
   <collapsible-accordion header="Organizations">
     <div class="mt-5">
       <fieldset>
-        <ul class="flex flex-row flex-wrap">
-          <li class="h-8 w-1/2">
-            <input id="VueTube" type="checkbox" class="mr-3 cursor-pointer" />
-            <label for="VueTube">VueTube</label>
-          </li>
-
-          <li class="h-8 w-1/2">
-            <input id="Beetween Vue and Me" type="checkbox" class="mr-3 cursor-pointer" />
-            <label for="Beetween Vue and Me">Beetween Vue and Me</label>
-          </li>
-
-          <li class="h-8 w-1/2">
-            <input id="Et vue Brute" type="checkbox" class="mr-3 cursor-pointer" />
-            <label for="Et vue Brute">Et vue Brute</label>
-          </li>
-
-          <li class="h-8 w-1/2">
-            <input id="Vue and a Half Men" type="checkbox" class="mr-3 cursor-pointer" />
-            <label for="Vue and a Half Men">Vue and a Half Men</label>
+        <ul class="flex flex-col columns-12">
+          <li v-for="organization in UNIQUE_ORGANIZATIONS" :key="organization" class="h-8 w-full">
+            <input :id="organization" type="checkbox" class="mr-3 cursor-pointer" />
+            <label :for="organization">{{ organization }}</label>
           </li>
         </ul>
       </fieldset>
