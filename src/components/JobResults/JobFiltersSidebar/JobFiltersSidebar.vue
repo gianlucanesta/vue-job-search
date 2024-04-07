e049396f35742d7db5a90844096cc59d95bd06a6
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-07 15:31:19 +0200

    Extracting Organizations Accordion to New Component

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -11,33 +11,7 @@
       <collapsible-accordion header="Job Types">
         <p>Placeholder for now</p>
       </collapsible-accordion>
-      <collapsible-accordion header="Organizations">
-        <div class="mt-5">
-          <fieldset>
-            <ul class="flex flex-row flex-wrap">
-              <li class="h-8 w-1/2">
-                <input id="VueTube" type="checkbox" class="mr-3 cursor-pointer" />
-                <label for="VueTube">VueTube</label>
-              </li>
-
-              <li class="h-8 w-1/2">
-                <input id="Beetween Vue and Me" type="checkbox" class="mr-3 cursor-pointer" />
-                <label for="Beetween Vue and Me">Beetween Vue and Me</label>
-              </li>
-
-              <li class="h-8 w-1/2">
-                <input id="Et vue Brute" type="checkbox" class="mr-3 cursor-pointer" />
-                <label for="Et vue Brute">Et vue Brute</label>
-              </li>
-
-              <li class="h-8 w-1/2">
-                <input id="Vue and a Half Men" type="checkbox" class="mr-3 cursor-pointer" />
-                <label for="Vue and a Half Men">Vue and a Half Men</label>
-              </li>
-            </ul>
-          </fieldset>
-        </div>
-      </collapsible-accordion>
+      <job-filters-sidebar-organizations />
     </section>
   </div>
 </template>
@@ -45,12 +19,14 @@
 <script>
 import ActionButton from '@/components/Shared/ActionButton.vue'
 import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
+import JobFiltersSidebarOrganizations from '@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue'
 
 export default {
   name: 'JobFiltersSidebar',
   components: {
     ActionButton,
-    CollapsibleAccordion
+    CollapsibleAccordion,
+    JobFiltersSidebarOrganizations
   }
 }
 </script>
---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -0,0 +1,39 @@
+<template>
+  <collapsible-accordion header="Organizations">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li class="h-8 w-1/2">
+            <input id="VueTube" type="checkbox" class="mr-3 cursor-pointer" />
+            <label for="VueTube">VueTube</label>
+          </li>
+
+          <li class="h-8 w-1/2">
+            <input id="Beetween Vue and Me" type="checkbox" class="mr-3 cursor-pointer" />
+            <label for="Beetween Vue and Me">Beetween Vue and Me</label>
+          </li>
+
+          <li class="h-8 w-1/2">
+            <input id="Et vue Brute" type="checkbox" class="mr-3 cursor-pointer" />
+            <label for="Et vue Brute">Et vue Brute</label>
+          </li>
+
+          <li class="h-8 w-1/2">
+            <input id="Vue and a Half Men" type="checkbox" class="mr-3 cursor-pointer" />
+            <label for="Vue and a Half Men">Vue and a Half Men</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </collapsible-accordion>
+</template>
+
+<script>
+import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
+export default {
+  name: 'JobFiltersSidebarOrganizations',
+  components: {
+    CollapsibleAccordion
+  }
+}
+</script>
