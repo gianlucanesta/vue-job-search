b398f4f5586612fc26a26474e9aef0620de2d69d
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-16 23:41:44 +0200

    Fixing Failing JobListings Tests

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -2,26 +2,19 @@ import { render, screen } from '@testing-library/vue'
 import { RouterLinkStub } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
 
+import { useRoute } from 'vue-router'
+vi.mock('vue-router')
+
 import JobListings from '@/components/JobResults/JobListings.vue'
 import { useJobsStore } from '@/stores/jobs'
 
 describe('JobListings', () => {
-  const createRoute = (queryParams = {}) => ({
-    query: {
-      page: '5',
-      ...queryParams
-    }
-  })
-
-  const renderJobListings = ($route) => {
+  const renderJobListings = () => {
     const pinia = createTestingPinia()
 
     render(JobListings, {
       global: {
         plugins: [pinia],
-        mocks: {
-          $route
-        },
         stubs: {
           RouterLink: RouterLinkStub
         }
@@ -30,19 +23,18 @@ describe('JobListings', () => {
   }
 
   it('fetches jobs', () => {
-    const $route = createRoute()
+    useRoute.mockReturnValue({ query: {} })
 
-    renderJobListings($route)
+    renderJobListings()
 
     const jobsStore = useJobsStore()
     expect(jobsStore.FETCH_JOBS).toHaveBeenCalled()
   })
 
   it('displays maximum of 10 jobs', async () => {
-    const queryParams = { page: '1' }
-    const $route = createRoute(queryParams)
+    useRoute.mockReturnValue({ query: { page: '1' } })
 
-    renderJobListings($route)
+    renderJobListings()
     const jobsStore = useJobsStore()
     jobsStore.jobs = Array(15).fill({})
 
@@ -52,10 +44,8 @@ describe('JobListings', () => {
 
   describe('when params exclude page number', () => {
     it('displays page number 1', () => {
-      const queryParams = { page: undefined }
-      const $route = createRoute(queryParams)
-
-      renderJobListings($route)
+      useRoute.mockReturnValue({ query: {} })
+      renderJobListings()
 
       expect(screen.getByText('Page 1')).toBeInTheDocument()
     })
@@ -63,10 +53,8 @@ describe('JobListings', () => {
 
   describe('when params include page number', () => {
     it('displays page number', () => {
-      const queryParams = { page: '3' }
-      const $route = createRoute(queryParams)
-
-      renderJobListings($route)
+      useRoute.mockReturnValue({ query: { page: '3' } })
+      renderJobListings()
 
       expect(screen.getByText('Page 3')).toBeInTheDocument()
     })
@@ -74,10 +62,9 @@ describe('JobListings', () => {
 
   describe('when user is on first page', () => {
     it('does not show link to previous page', async () => {
-      const queryParams = { page: '1' }
-      const $route = createRoute(queryParams)
+      useRoute.mockReturnValue({ query: { page: '1' } })
 
-      renderJobListings($route)
+      renderJobListings()
       const jobsStore = useJobsStore()
       jobsStore.jobs = Array(15).fill({})
 
@@ -87,10 +74,9 @@ describe('JobListings', () => {
     })
 
     it('shows link to next page', async () => {
-      const queryParams = { page: '1' }
-      const $route = createRoute(queryParams)
+      useRoute.mockReturnValue({ query: { page: '1' } })
 
-      renderJobListings($route)
+      renderJobListings()
       const jobsStore = useJobsStore()
       jobsStore.jobs = Array(15).fill({})
 
@@ -102,10 +88,9 @@ describe('JobListings', () => {
 
   describe('when user is on last page', () => {
     it('does not show link to next page', async () => {
-      const queryParams = { page: '2' }
-      const $route = createRoute(queryParams)
+      useRoute.mockReturnValue({ query: { page: '2' } })
 
-      renderJobListings($route)
+      renderJobListings()
       const jobsStore = useJobsStore()
       jobsStore.jobs = Array(15).fill({})
 
@@ -115,10 +100,9 @@ describe('JobListings', () => {
     })
 
     it('shows link to previous page', async () => {
-      const queryParams = { page: '2' }
-      const $route = createRoute(queryParams)
+      useRoute.mockReturnValue({ query: { page: '2' } })
 
-      renderJobListings($route)
+      renderJobListings()
       const jobsStore = useJobsStore()
       jobsStore.jobs = Array(15).fill({})
 
