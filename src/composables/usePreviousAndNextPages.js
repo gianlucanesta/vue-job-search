6b4fc8a7cbfc0ab38642b183496bd5f751c647f3
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-17 22:10:00 +0200

    Extracting Previous and Next Page Logic to Composable Function

---
+++ src/composables/usePreviousAndNextPages.js
@@ -0,0 +1,21 @@
+import { computed } from 'vue'
+
+const usePreviousAndNextPages = (currentPage, maxPage) => {
+  const previousPage = computed(() => {
+    if (currentPage.value > 1) {
+      return currentPage.value - 1
+    } else {
+      return null
+    }
+  })
+
+  const nextPage = computed(() => {
+    const nextPage = currentPage.value + 1
+    const maxPage = Math.ceil(FILTERED_JOBS.value.length / 10)
+    return currentPage.value < maxPage.value ? nextPage : undefined
+  })
+
+  return { previousPage, nextPage }
+}
+
+export default usePreviousAndNextPages
