320e6dc8170306ba0d0fd2da4d27d680a5758c15
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-20 14:27:47 +0200

    Update getJobs API Call

--- src/api/getJobs.ts
+++ src/api/getJobs.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+import type { Job } from '@/api/types'
+
 const getJobs = async () => {
   const baseUrl = import.meta.env.VITE_APP_API_URL
   const url = `${baseUrl}/jobs`
