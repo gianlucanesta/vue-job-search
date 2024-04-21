5f30a3dfa920fa6a401df188db1f9b3eda142172
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-21 17:51:00 +0200

    TDD for API Call: getDegrees API Call

--- src/api/getJobs.ts
+++ src/api/getJobs.ts
@@ -5,7 +5,7 @@ import type { Job } from '@/api/types'
 const getJobs = async () => {
   const baseUrl = import.meta.env.VITE_APP_API_URL
   const url = `${baseUrl}/jobs`
-  const response = await axios.get(url)
+  const response = await axios.get<Job[]>(url)
 
   return response.data
 }
