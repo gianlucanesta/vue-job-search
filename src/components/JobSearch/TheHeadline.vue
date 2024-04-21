62826dd538043a65581a2239396decded4d625fe
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-21 13:28:55 +0200

    Adding Type Annotations to TheHeadline Component

--- src/components/JobSearch/TheHeadline.vue
+++ src/components/JobSearch/TheHeadline.vue
@@ -8,38 +8,63 @@
   </section>
 </template>
 
-<script>
+<script lang="ts" setup>
 import nextElementInList from '../../utils/nextElementInList'
-export default {
-  name: 'TheHeadline',
-  data() {
-    return {
-      action: 'Build',
-      interval: null
-    }
-  },
-  computed: {
-    actionClasses() {
-      return {
-        [this.action.toLowerCase()]: true
-      }
-    }
-  },
-  created() {
-    this.changeTitle()
-  },
-  beforeUnmount() {
-    clearInterval(this.interval)
-  },
-  methods: {
-    changeTitle() {
-      this.interval = setInterval(() => {
-        const actions = ['Build', 'Create', 'Design', 'Code']
-        this.action = nextElementInList(actions, this.action)
-      }, 3000)
-    }
+
+import { ref, computed, onMounted, onBeforeMount } from 'vue'
+
+const action = ref('Build')
+const interval = ref<ReturnType<typeof setInterval>>()
+
+const actionClasses = computed(() => {
+  return {
+    [action.value.toLowerCase()]: true
   }
+})
+
+const changeTitle = () => {
+  interval.value = setInterval(() => {
+    const actions = ['Build', 'Create', 'Design', 'Code']
+    action.value = nextElementInList(actions, action.value)
+  }, 3000)
 }
+
+onMounted(changeTitle)
+
+onBeforeMount(() => {
+  clearInterval(interval.value)
+})
+
+// export default {
+//   name: 'TheHeadline',
+//   data() {
+//     return {
+//       action: 'Build',
+//       interval: null
+//     }
+//   },
+//   computed: {
+//     actionClasses() {
+//       return {
+//         [this.action.toLowerCase()]: true
+//       }
+//     }
+//   },
+//   created() {
+//     this.changeTitle()
+//   },
+//   beforeUnmount() {
+//     clearInterval(this.interval)
+//   },
+//   methods: {
+//     changeTitle() {
+//       this.interval = setInterval(() => {
+//         const actions = ['Build', 'Create', 'Design', 'Code']
+//         this.action = nextElementInList(actions, this.action)
+//       }, 3000)
+//     }
+//   }
+// }
 </script>
 <style scoped>
 .build {
