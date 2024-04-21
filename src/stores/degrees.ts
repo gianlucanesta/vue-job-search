0e7eb184008dede9a96ab1efcced99e09efa71de
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-04-21 18:13:42 +0200

    TDD for Action: FETCH_DEGREES

--- src/stores/degrees.ts
+++ src/stores/degrees.ts
@@ -2,11 +2,19 @@ import { ref } from 'vue'
 
 import { defineStore } from 'pinia'
 
+import getDegrees from '../api/getDegrees'
 import type { Degree } from '@/api/types'
 
 export const useDegreesStore = defineStore('degrees', () => {
   const degrees = ref<Degree[]>([])
+
+  const FETCH_DEGREES = async () => {
+    const receivedDegrees = await getDegrees()
+    degrees.value = receivedDegrees
+  }
+
   return {
-    degrees
+    degrees,
+    FETCH_DEGREES
   }
 })
--- tests/unit/stores/degrees.test.ts
+++ tests/unit/stores/degrees.test.ts
@@ -1,6 +1,13 @@
+import type { Mock } from 'vitest'
+
 import { createPinia, setActivePinia } from 'pinia'
+import axios from 'axios'
 
 import { useDegreesStore } from '@/stores/degrees'
+
+const axiosGetMoc = axios.get as Mock
+
+vi.mock('axios')
 
 describe('state', () => {
   beforeEach(() => {
@@ -9,5 +16,33 @@ describe('state', () => {
   it('stores all degrees that jobs may require', () => {
     const store = useDegreesStore()
     expect(store.degrees).toEqual([])
+  })
+})
+
+describe('actions', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+  describe('FETCH_DEGREES', () => {
+    it('maks API request and stores received degrees', async () => {
+      axiosGetMoc.mockResolvedValue({
+        data: [
+          {
+            id: 1,
+            degree: "Master's"
+          }
+        ]
+      })
+
+      const store = useDegreesStore()
+      await store.FETCH_DEGREES()
+
+      expect(store.degrees).toEqual([
+        {
+          id: 1,
+          degree: "Master's"
+        }
+      ])
+    })
   })
 })
